ac31ce1a61af0a0181803327cc55393c6945bd71
Mahesh Gaikwad <gaikwadmahesh777g@gmail.com>
2021-11-25 23:30:21 +0530

    Started With Code

---
+++ StudentManagementSystem.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<stdlib.h>
+#include<cstring>
+#include<stdio.h>
+#include<fstream>
+#include<conio.h>
+char un[20];
+class Admin
+{
+    char name[20];
+    int totsub;
+    char subject[10] [10];
+    char mobile[15],mail[50],fname[20];
+    char passwd[20],rpasswd[20];
+    
+};
