fce483390d525d7e88c6557a07e6d029396c8371
Mahesh Gaikwad <gaikwadmahesh777g@gmail.com>
2021-11-26 18:36:27 +0530

    Updated the code

--- StudentManagementSystem.cpp
+++ StudentManagementSystem.cpp
@@ -95,10 +95,120 @@ class Admin
             cout<< "\nFaculty Name "<<name;
             cout<<" \nFaculty Father's Name "<<fname; 
             cout<<"\nFaculty Mobile No. "<<mobile;
-            cout<<" \nFacul ty E-Mail ID"<<mail; 
+            cout<<" \nFaculty E-Mail ID"<<mail; 
             return 1; 
             }
             else 
             return 0; 
         }
+        //This function displays the different subjects of the faculty at their Respective Portals
+
+        int knowfasub(){
+            if((strcmp(::un,rollno)==0)){
+                cout<<"\nFaculty Total Subjects : "<<totsub;
+                for(int i=0;i<totsub;i++){
+                    cout<<"\n\tSubject"<<i+1<<" : "<<subject[i];
+                }
+                return 1;
+            }
+            else{
+                return 0;
+            }
+        }
+        //this function helps a facultty to add a subject in their module. 
+        void addfasub(){
+            if ((strcmp(:: un, rollno) ==0) ){
+            cout<<"\nEnter the New Subject : ";
+            cin>>subject[totsub];
+            totsub++; 
+            cout<<"\n\nNew Subject Added Successful1ly. . ."; 
+            }
+        }
+        //this fuction helps a faculty to delete a subject from their module.
+        void delfasub (){
+            knowfasub (); 
+            int de=0; 
+            if((strcmp (::un,rollno) ==0)){
+                if (totsub==0 || totsub<0){
+                    totsub=0; 
+                    cout<<"\nNone Subjects Exist. .."; 
+                    getch ();
+                    exit(0); 
+                }
+                cout<<" \nEnter the Subject No. to be Deleted: ";
+                cin>>de; 
+                if (de==totsub) {
+                    totsub--;
+                    strcpy(subject [totsub], " ") ;
+                }
+                else if (totsub==1) {
+                    totsub=0; 
+                    strcpy (subject [totsub], " ");
+                }
+                else{
+                  de--;
+                  strcpy(subject[totsub]," ");
+                  for(int p=de;p<totsub;p++){
+                      strcpy(subject[p],subject[p+1]);
+                  } 
+                  totsub--;
+                }
+                cout<<"\nRecords Updated Successfully...";
+            }
+        }
+        //this function helps a faculty to modify his personal profile
+        void modfaprofile(){
+            if((strcmp (::un,rollno) ==0)){
+                cout<<"\nThe Profile Details are : ";
+                cout<<" \n 1. Faculty E-Mail : "<<mail; 
+                cout<<" \n 2. Facultty Mobile: "<<mobile; 
+                int g= -1 ; 
+                cout<<" \n\nEnter the Detail No. to be Modified:" ;
+                cin>>g;
+                if(g==1){
+                    char nmail [50]; 
+                    cout<<" \nEnter the New Mail Address:";
+                    strcpy (mail, nmail) ; 
+                    cout<<" \nRecords Updated Successfully."; 
+                }
+                else if(g==2){
+                    char nmobile [15] ; 
+                    cout<< " \nEnter the New Mobile No. "; 
+                    gets (nmobile) ; 
+                    strcpy (mobile, nmobile); 
+                    cout<< " \nRecords Updated Successfully. .. ";
+                }
+                else{
+                    cout<<"\nInvalid Input Provided...";
+                }
+            }
+        }
+        //This function displays the students profile at the students portal
+        int stprofile(){
+            if((strcmp (::un,rollno) ==0)){
+                cout<<" \nStudent Name :"<<name; 
+                cout<< " \nStudent Father 's Name : "<<fname; 
+                cout<< " \nStudent Mobile No.  "<<mobile; 
+                cout<<" \nStudent E-Mail ID "<<mail; 
+                return 1;
+            }
+            else{
+                return 0;
+            }
+        } 
+        //This function displays the different enrolled subjects of the respective students ...
+        // according to their profile.
+        int knowstsub(){
+          if((strcmp (::un,rollno) ==0)){
+              cout<<"\nStudents Total Subjects : "<<totsub;
+              for(int i=0;i<totsub;i++){
+                  cout<<"\n\tSubject "<<i+1<<" : "<<subject[i];
+              }
+              return 1;
+        }else{
+            return 0;
+        }
+    }
+    // this function allows the student to add a subiects in total subjects of the student profile..
+    
 };
